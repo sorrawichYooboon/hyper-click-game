061fc0bcc18687c0dd7e6cf1c1b9b0d5aa92bfd4
Sorrawich Yooboon <sorrawich.y@gmail.com>
2023-10-14 23:07:06 +0700

    feat:init threejs

--- src/pages/HomePage/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Homepage: React.FC = () => {
-  return <h1 className="text-3xl font-bold underline">Home page</h1>;
-};
-
-export default Homepage;
--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import { Suspense, lazy } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import LoadingPage from "src/pages/LoadingPage";
 
 const lazyLoad = (Component: any) => (props: any) =>
@@ -9,14 +9,23 @@ const lazyLoad = (Component: any) => (props: any) =>
     </Suspense>
   );
 
-const Homepage = lazyLoad(lazy(() => import("src/pages/HomePage")));
+const GamesPage = lazyLoad(lazy(() => import("src/pages/GamesPage")));
+const HyperClickGame = lazyLoad(
+  lazy(() => import("src/pages/GamesPage/HyperClickGame"))
+);
+const HyperTypingGame = lazyLoad(
+  lazy(() => import("src/pages/GamesPage/HyperTypingGame"))
+);
 
 export default function Router() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<Navigate to="/games" />} />
         <Route path="/loading" element={<LoadingPage />} />
+        <Route path="/games" element={<GamesPage />} />
+        <Route path="/games/hyper-click" element={<HyperClickGame />} />
+        <Route path="/games/hyper-typing" element={<HyperTypingGame />} />
       </Routes>
     </>
   );
