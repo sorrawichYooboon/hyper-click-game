842e38fbd78578b3521e68cef42308ccd432ba36
Sorrawich Yooboon <sorrawich.y@gmail.com>
2023-11-21 03:11:35 +0700

    chore:comment lazy load

--- src/router.tsx
+++ src/router.tsx
@@ -1,23 +1,24 @@
-import { Suspense, lazy } from "react";
+// import { Suspense, lazy } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import LoadingPage from "src/pages/LoadingPage";
+// import LoadingPage from "src/pages/LoadingPage";
+import HyperClickGame from "src/pages/HyperClickGame";
 
-const lazyLoad = (Component: any) => (props: any) =>
-  (
-    <Suspense fallback={<LoadingPage />}>
-      <Component {...props} />
-    </Suspense>
-  );
+// const lazyLoad = (Component: any) => (props: any) =>
+//   (
+//     <Suspense fallback={<LoadingPage />}>
+//       <Component {...props} />
+//     </Suspense>
+//   );
 
 // const GamesPage = lazyLoad(lazy(() => import("src/pages")));
-const HyperClickGame = lazyLoad(lazy(() => import("src/pages/HyperClickGame")));
+// const HyperClickGame = lazyLoad(lazy(() => import("src/pages/HyperClickGame")));
 
 export default function Router() {
   return (
     <>
       <Routes>
         <Route path="*" element={<Navigate to="/hyper-click-game" />} />
-        <Route path="/loading" element={<LoadingPage />} />
+        {/* <Route path="/loading" element={<LoadingPage />} /> */}
         {/* <Route path="/" element={<GamesPage />} /> */}
         <Route path="/hyper-click-game" element={<HyperClickGame />} />
       </Routes>
